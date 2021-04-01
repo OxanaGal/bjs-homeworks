86bae53b5945aca70ea2abf95c0858bd65bc1fb9
Oxana Galyamova <mrs.oxas@gmail.com>
2021-04-01 19:59:53 +0500

    Initial commit

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,27 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
+    if (typeof percent === 'Nan' || contribution === 'Nan' || amount === 'Nan') {
+        return `Неверное значение параметра`;
+    }
+    
+    let primaryLoan = amount - contribution;
+    let monthAmount = (date.getFullYear() - new Date().getFullYear()) * 12 - new Date().getMonth() + date.getMonth();
+    let monthPercent = percent / 100 / 12 ;
+    let monthlyPay = primaryLoan *(monthPercent + monthPercent /(((1 + monthPercent)**monthAmount)-1));
+    let totalAmount = monthlyPay * monthAmount;
+    return Number(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting;
+    if (typeof name == 'undefined' || !name.length) {
+        greeting = 'Аноним';
+    } else {
+        greeting = name;
+    }
+    console.log(greeting);
+    return `Привет, мир! Меня зовут ${greeting}.`;
 }
