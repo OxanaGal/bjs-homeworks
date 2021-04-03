6f91e6b8f89f77576ee09d31803a626f7b9c47c8
Oxana Galyamova <mrs.oxas@gmail.com>
2021-04-04 00:02:44 +0500

    update 1.1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -2,9 +2,21 @@
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    if (typeof percent === 'Nan' || contribution === 'Nan' || amount === 'Nan') {
-        return `Неверное значение параметра`;
-    }
+    if (percent.isNaN || !percent.length || percent <= 0 || percent > 100) {
+        alert `Параметр «Процентная ставка» содержит неправильное значение`;
+    };
+    
+    if (contribution.isNaN || !percent.length || contribution < 0) {
+        alert `Параметр «Процентная ставка» содержит неправильное значение`;
+    };
+    
+    if (amount.isNaN || !percent.length || amount <= 0) {
+        alert `Параметр «Общая стоимость» содержит неправильное значение`;
+    };
+    
+    if (date.isNaN || date < new Date()) {
+        alert `Параметр «Срок ипотеки» содержит неправильное значение`;
+    };
     
     let primaryLoan = amount - contribution;
     let monthAmount = (date.getFullYear() - new Date().getFullYear()) * 12 - new Date().getMonth() + date.getMonth();
@@ -12,7 +24,7 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     let monthlyPay = primaryLoan *(monthPercent + monthPercent /(((1 + monthPercent)**monthAmount)-1));
     let totalAmount = monthlyPay * monthAmount;
     return Number(totalAmount.toFixed(2));
-}
+};
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
