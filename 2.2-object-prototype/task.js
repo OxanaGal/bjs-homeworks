c743581ba21af5a0e2dfd8e11acc82cf893683b3
Oxana Galyamova <mrs.oxas@gmail.com>
2021-05-20 00:00:42 +0500

    v 1.0

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,42 @@
-//String.prototype.isPalindrome - для задачи №1
+'use strict';
+
+String.prototype.isPalindrome = function isPalindrome(){
+    let newString = this.toLocaleLowerCase().split(" ").join("");
+    console.log(newString);
+    // reverse работает только с массивами, а split возвращает массив. 
+    // Может костыльно, но вроде работает :D 
+    let newStringReversed = newString.split("").reverse().join("");
+    console.log(newStringReversed);
+
+    return newString === newStringReversed;
+};
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
-    // return averageMark
-}
+    let sumMarks = 0;
+    
+    if (marks.length === 0) {
+        return 0;
+    };
+       
+    for (let i = 0; i < marks.length; i++) {
+        sumMarks += marks[i];
+      };
+    
+    let roundedAverage = Math.round(sumMarks / marks.length);
+   
+    return roundedAverage;
+};
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
-    // return verdict
-}
+    let now = Date.now();
+    birthday = Number(new Date(birthday));
+
+    let diff = now - birthday;
+    console.log(diff)
+    // округлим дополнительно, возраст обычно целое число
+    let age = Math.floor(diff / (1000 * 3600 * 24 * 365.2425));
+    console.log(age);
+    return (age > 18);
+};
